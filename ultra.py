63c76338e48274e3f28e1b5ad586e7a9db46df82
Ed Campbell <drescampbell@gmail.com>
2015-12-18 22:16:43 +0000

    More stuff

--- ultra.py
+++ ultra.py
@@ -4,12 +4,20 @@ me explore cloud technologies. It begins by
 listing a store a  gpx files.
 
 """
-from flask import Flask, g
+import dateutil.parser
+from flask import Flask, g, request, render_template, redirect, flash, url_for
 from pymongo import MongoClient
 
 
 app = Flask(__name__)
 
+app.config.update(dict(
+#    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+#    DEBUG=True,
+    SECRET_KEY='development key',
+#    USERNAME='admin',
+#    PASSWORD='default'
+))
 
 def connect_db():
     """
@@ -36,13 +44,26 @@ def get_db():
 def show_runs():
     db = get_db()
     collection = db.runs
-    entries = collection.find()
-    val = ''
-    for run in entries:
-        val += str(run)
+    entries = [dict(title=entry['file'], text=str(entry['dt'])) for
+               entry in collection.find()]
+    return  render_template('show_runs.html', entries=entries)
 
-    return  val
-    #return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+
+    try:
+        dt = dateutil.parser.parse(request.form['text'])
+    except ValueError:
+        flash('Failed to parse datetime')
+    else:
+        db = get_db()
+        db.runs.insert_one({'file': request.form['title'],
+                            'dt': dt})
+        flash('New entry was successfully posted')
+
+    return redirect(url_for('show_runs'))
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
